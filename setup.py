f02d0a3a9377b6a509e16aa26f65a667b1522482
Developer <admin@73rdstreetdevelopment.co.uk>
2017-05-29 15:18:10 +0100

    4.0.0 - setup.py commit.

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
+'''------------------------------------------------------------------------------------------------
+Program:    setup.py
+Version:    0.0.1
+Py Ver:     2.7
+Purpose:    Setup packager for utils.
+
+Dependents: distutils
+            setuptools
+
+Comments:
+
+            Installation:
+            > cd /path/to/package_x.x.x
+            > pip install . --no-deps
+
+---------------------------------------------------------------------------------------------------
+UPDATE LOG:
+Date        Programmer      Version     Update
+22.03.17    J. Berendt      0.0.1       Written
+-------------------------------------------------------------------------------------------------'''
 
 import sys
 import os
@@ -14,18 +34,18 @@ PREFIX = os.path.realpath(sys.prefix).lower()
 INST_ROOT = os.path.realpath(INST_RAW).lower().replace(PREFIX, '.')
 
 #DEFINE PARAMETERS (LIST PROGRAM DEPENDENCIES IN INSTALL_REQUIRES PARAMETER)
-params = dict(  prog=PACKAGE,
-                version=__version__,
-                platforms='Python 2.7',
-                description='Bespoke general utilities package for Python 2.7',
-                name='J. Berendt',
-                email='support@73rdstreetdevelopment.co.uk',
-                url='https://73rdstreetdevelopment.wordpress.com',
-                license='MIT',
-                packages=find_packages(),
-                install_requires=['numpy', 'cx_Oracle', 'unidecode', 'matplotlib', 'pyodbc', 'plotly'],
-                data_files=[(INST_ROOT, ['README.html', 'README.md', 'LICENSE'])]
-                )
+params = dict(prog=PACKAGE,
+              version=__version__,
+              platforms='Python 2.7',
+              description='Bespoke general utilities package for Python 2.7',
+              name='J. Berendt',
+              email='support@73rdstreetdevelopment.co.uk',
+              url='https://73rdstreetdevelopment.wordpress.com',
+              license='MIT',
+              packages=find_packages(),
+              install_requires=['numpy', 'cx_Oracle', 'unidecode', 'matplotlib', 'pyodbc', 'plotly'],
+              data_files=[(INST_ROOT, ['readme/README.html', 'readme/README.md', 'LICENSE'])]
+              )
 
 #SETUP PARAMETERS
 setup(name=params['prog'],
