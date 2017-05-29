de6ec362109e1d820c732364b8847bc890b666c3
Developer <admin@73rdstreetdevelopment.co.uk>
2017-05-29 14:49:27 +0100

    4.0.0 - reporterror 0.0.2 update commit.

--- utils/reporterror.py
+++ utils/reporterror.py
@@ -1,6 +1,6 @@
 '''------------------------------------------------------------------------------------------------
 Program:    reporterror
-Version:    0.0.1
+Version:    0.0.2
 Platform:   Windows / Linux
 Py Ver:     2.7
 Purpose:    Module designed to report program errors to the console
@@ -15,20 +15,27 @@ Email:      support@73rdstreetdevelopment.co.uk
 
 Comments:
 
-Use:        >
+Use:        > import utils.reporterror as reporterror
             >
+            > try:
+            >   stuff here ...
+            > except Exception as err:
+            >   #SEND ERROR TO REPORTERROR METHOD
+            >   reporterror.reporterror(err)
 
 ---------------------------------------------------------------------------------------------------
 UPDATE LOG:
 Date        Programmer      Version     Update
 01.01.17    J. Berendt      0.0.1       Written. pylint (10/10)
+29.05.17    J. Berendt      0.0.2       Updated to fit within the installed utils module.
+                                        Added the CMD to log file output.  pylint (10/10)
 ------------------------------------------------------------------------------------------------'''
 
 from _version_reporterror import __version__
 
 #-----------------------------------------------------------------------
 #METHOD USED TO REPORT AN ERROR
-def reporterror(error):
+def reporterror(error, logevent=False, logfilepath='c:/temp/reporterror.log'):
 
     '''
     DESIGN:
@@ -37,13 +44,19 @@ def reporterror(error):
     PARAMETERS:
         - error
         Exception from the error handler; refer to the USE section.
+        - logevent (default=False)
+        Send the error to a log file.
+        Note: use of this command assumes the log file is already
+        created, and the header is already written.
+        - logfilename (default='c:/temp/reporterror.log')
+        File path to the log file.
 
     USE:
     > try:
     >     stuff here ...
     > except Exception as err:
     >     #REPORT / LOG ERROR
-    >     reporterror(err)
+    >     reporterror.reporterror(err)
     '''
 
     import sys
@@ -63,7 +76,11 @@ def reporterror(error):
     print 'CMD:\t%s'    % text
 
     #LOG ERROR
-    #w2l(CONFIG['file_log'], 'ERROR: %s; METHOD: %s; LINE: %s' % (error, func_name, line_num))
+    if logevent: w2l(filepath=logfilepath,
+                     text='ERROR: %s; CMD: %s; METHOD: %s; LINE: %s' % (error, text,
+                                                                        func_name,
+                                                                        line_num),
+                     autofill=True)
 
     #CLEANUP
     del (exc_type, exc_obj, exc_tb)
