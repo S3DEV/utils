04b4183860c0ca51fcc6dd93a1cea7ca5f08817b
Jeremy Berendt <berendtj@RRDA.net>
2017-07-20 14:40:20 +0100

    4.3.0 - Added a Log class with write() and write_blank_line() methods to replace the write2log() method. Added warnings indicating that write2log has been depreciated and will be removed in future utils versions.

--- test/test_log.py
+++ test/test_log.py
@@ -1,6 +1,6 @@
 '''------------------------------------------------------------------------------------------------
 Program:    test_log
-Version:    0.0.1
+Version:    0.1.0
 Py Ver:     2.7
 Purpose:    Unit test for utils.log
 
@@ -19,6 +19,8 @@ Use:        > cd /package_root/test
 UPDATE LOG:
 Date        Programmer      Version     Update
 29.05.17    J. Berendt      0.0.1       Written
+20.07.17    J. Berendt      0.1.0       Updated to test the write() method of the Log class rather
+                                        than write2log().
 ------------------------------------------------------------------------------------------------'''
 
 import os
@@ -27,7 +29,7 @@ import unittest
 
 #ADD DIR PATH TO IMPORT UTILS.LOG
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-import utils.log as log
+from utils.log import Log
 
 
 class TestLog(unittest.TestCase):
@@ -37,36 +39,36 @@ class TestLog(unittest.TestCase):
         import re
 
         #VARIABLES
-        _log_path       = 'c:/temp/utils_log_unittest.log'
-        _header         = 'datetime,host,user,text'
-        _entry          = 'this is a test'
-        _pattern_entry  = r'([0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{6},' \
+        log_path       = 'c:/temp/utils_log_unittest.log'
+        header         = 'datetime,host,user,text'
+        entry          = 'this is a test'
+        pattern_entry  = r'([0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{6},' \
                           '[0-9a-zA-Z].*,' \
-                          '[0-9a-zA-Z].*,' + _entry + ')'
-        _exp            = re.compile(_pattern_entry)
-        _match          = []
+                          '[0-9a-zA-Z].*,' + entry + ')'
+        exp            = re.compile(pattern_entry)
+
+        #INSTANTIATE LOG CLASS
+        _log = Log(filepath=log_path, autofill=True, printheader=True,
+                   headertext=header)
 
         #WRITE LOG FILE
-        log.write2log(filepath=_log_path, text=_entry,
-                      autofill=True, printheader=True,
-                      headertext=_header)
+        _log.write(text=entry)
+        _log.write_blank_line()
 
         #READ IN LOG FILE
-        text = [line.strip() for line in open(_log_path, 'r').readlines()]
+        text = [line.strip() for line in open(log_path, 'r').readlines()]
 
         #TEST HEADER
-        self.assertTrue(text[0] == _header)
+        self.assertTrue(text[0] == header)
 
-        #LOOP OVER LOG TEXT AND MATCH PATTERN USING REGEX (SKIP HEADER)
-        for line in text[1:]:
-            #ADD TEST RESULT TO LIST
-            _match.append(True if len(_exp.findall(line)) > 0 else False)
+        #TEST LOG ENTRY AGAINST REGEX
+        self.assertTrue(len(exp.findall(text[1])) == 1)
 
-        #TEST LIST FOR ANY FALSE VALUES (INDICATING A LINE FAILED THE TEST)
-        self.assertTrue(False not in _match)
+        #TEST FOR BLANK LINE (WRITTEN BY write_blank_line() METHOD)
+        self.assertTrue(text[2] == '')
 
         #DELETE THE LOG FILE
-        if os.path.exists(_log_path): os.remove(_log_path)
+        if os.path.exists(log_path): os.remove(log_path)
 
 
 #-----------------------------------------------------------------------
--- utils/log.py
+++ utils/log.py
@@ -1,6 +1,6 @@
 '''------------------------------------------------------------------------------------------------
 Program:    log
-Version:    0.1.1
+Version:    0.2.0
 Py Ver:     2.7
 Purpose:    Small program designed to be a central log file creator.
             Updated to be part of the utils package, and installed into site-packages.
@@ -21,8 +21,10 @@ Email:      support@73rdstreetdevelopment.co.uk
 
 Comments:
 
-Use:        >>> from utils.log import write2log as w2l
-            >>> w2l(filepath, text, [printheader=False], [headertext=''])
+Use:        >>> from utils.log import Log
+            >>> _log = Log(filepath='path/to/file.log')
+            >>>
+            >>> _log.write(text='most cows can just over the moon,Fact,94.2,pct')
 
 ---------------------------------------------------------------------------------------------------
 UPDATE LOG:
@@ -42,45 +44,56 @@ Date        Programmer      Version     Update
                                         Code revised / cleaned to meet PEP-8 style standards.
                                         pylint (10/10)
 29.05.17    J. Berendt      0.1.1       Updated to add new line before UserWarning errors.
+19.07.17    J. Berendt      0.2.0       Added a Log class with write() and write_blank_line()
+                                        methods to replace the write2log() method.
+                                        Added warnings indicating that write2log has been
+                                        depreciated and will be removed in future utils versions.
+                                        pylint (10/10)
 ------------------------------------------------------------------------------------------------'''
 
+import os
+import socket
+import getpass
+from datetime import datetime as dt
+
+import reporterror
 from _version_log import __version__
 
-#-----------------------------------------------------------------------
-def write2log(filepath, text, autofill=True, printheader=False,
-              headertext=''):
+
+class Log(object):
 
     '''
     DESIGN:
-    Designed as a small external logging program.
+    This is a small and simple log file writer class.
 
     The calling program is responsible for passing the proper arguments
     to create the log file header.
-    (i.e.: PrintHeader=True, HeaderText='some, header, text, here')
+    (i.e.: printheader=True, headertext='some,header,text,here')
 
-    It is suggested to call write2log() at program startup, with the
-    printheader argument set to True, and a header string passed.
-    If the log file already exists, the header will not be written.
+    On class instantiation, validation is performed to determine if the
+    log file exists.  If the log file does not exist, a header is
+    required before writing to the log file.  These parameters can be
+    passed to the class on instantiation, and will be ignored if the
+    log file already exists.
 
     PARAMETERS:
         - filepath
-        Path to the log file.
-        - text
-        Entry text to be written to the log.  The format should follow
-        the  format of the header.
+        Full path to the log file.
         - autofill (default=True)
         Auto-fill the log entry with datetime(now), host and username
         values.
         - printheader (default=False)
         Flag to print the text passed to the headertext parameter as
-        the log file header.  This is typically only writte on file
-        creation.
+        the log file header.
         - headertext (default='')
-        [Comma separated] values to be written as the log file header.
+        Values to be written as the log file header.
+        - sep (default=',')
+        Separator to be used in the log file.
+        This is used by the class when autofill is True.
 
     FILE VALIDATION:
-    Tests are performed to ensure the log file is being populated
-    correctly.
+    On class instantiation, tests are performed to ensure the log file
+    is being populated in a logical way.
     1) If printheader is False, and the log file does not yet exist,
     the user is notified.
     2) If printheader is True, yet headertext is blank, the user is
@@ -89,18 +102,153 @@ def write2log(filepath, text, autofill=True, printheader=False,
     the header will not be written.
 
     USE:
-    > from utils.log import write2log as w2l
-    > log(filepath, text, [autofill=True], [printheader=False],
-          [headertext=''])
+    > from utils.log import Log
+    > _log = Log(filepath='path/to/file.log', autofill=True)
+    >
+    > _log.write(text='most cows can just over the moon,Fact,94.2,pct')
     '''
 
-    from datetime import datetime as dt
-    import os
-    import socket
-    import getpass
+    #-------------------------------------------------------------------
+    #INITIALISATION METHOD
+    def __init__(self, filepath, autofill=True, printheader=False, headertext='', sep=','):
+
+        #INITIALISE CLASS VARIABLES
+        self._filepath  = filepath
+        self._autofill  = autofill
+        self._sep       = sep
+        self._host      = socket.gethostname()
+        self._user      = getpass.getuser()
+        self._autotext  = ('%s%s%s%s' % (self._host, self._sep, self._user, self._sep)
+                           if self._autofill is True else '')
+
+        #VALIDATION
+        #TEST THE LOG FILE EXISTS (IF HEADER IS NOT REQUESTED)
+        if printheader is False and os.path.exists(self._filepath) is False:
+            #NOTIFY USER
+            raise UserWarning('The log file does not exist, however a header was not requested. '
+                              'A header must be written at the time of log file creation.\n')
+
+
+        #VALIDATION
+        #TEST PRINTHEADER ARGUMENT, TO ENSURE A HEADER STRING IS BEING PASSED >> RAISE ERROR
+        if printheader is True and headertext == '':
+            #NOTIFY USER
+            raise UserWarning('The printheader argument is True, however the headertext string is '
+                              'blank. A headertext string must also be supplied.\n')
+
+        #WRITE HEADER
+        #TEST TO BE SURE A HEADER SHOULD BE WRITTEN >> PRINT HEADER
+        if printheader is True and headertext != '' and os.path.exists(self._filepath) is False:
+            #CREATE FILE
+            with open(self._filepath, 'a') as f:
+                #WRITE HEADER
+                f.write(headertext)
+                #ADD NEW LING CHARACTER
+                f.write('\n')
+
+
+    #-------------------------------------------------------------------
+    #METHOD FOR WRITING TO THE LOG FILE
+    def write(self, text):
+
+        '''
+        DESIGN:
+        The write() method is used to write text to a log file.
+
+        The log file name is defined on class instantiation.
+
+        If the autofill argument is True, the current datetime, host
+        and username values are written (in that order), ahead of the
+        text string provided to the text artument.
+
+        PARAMETERS:
+        - text
+        Text string to be written to the log.
+        Note that is autofill is True, the datetime, host and username
+        values will be populated, and *do not* need to be passed in this
+        string.
+
+        USE:
+        > from utils.log import Log
+        > _log = Log(filepath='path/to/file.log', autofill=True)
+        >
+        > _log.write(text='just adding some random text to my log')
+        '''
+
+        try:
+
+            #TEST THAT TEXT IS PASSED >> WRITE TEXT TO LOG
+            if text != '':
+
+                #TEST FOR AUTOFILL >> GET / POPULATE TIMESTAMP
+                auto_text = '%s%s%s' % (dt.now(),
+                                        self._sep,
+                                        self._autotext) if self._autofill is True else ''
+
+                #APPEND TEXT TO LOG FILE
+                with open(self._filepath, 'a') as f:
+                    #WRITE TEXT
+                    f.write(auto_text)
+                    f.write(text)
+                    #ADD NEW LINE CHARACTER
+                    f.write('\n')
+
+        except Exception as err:
+            #NOTIFICATION
+            reporterror.reporterror(err)
+
+
+    #-------------------------------------------------------------------
+    #METHOD FOR WRITING A SINGLE BLANK LINE TO THE LOG FILE
+    def write_blank_line(self):
+
+        r'''
+        PURPOSE:
+        The write_blank_lines() method does exactly as it says; writes
+        a single blank line to the log.
+
+        Note, that the autofill information *is not* written.  This is
+        a true blank line, created by writing a '\n' value to the log.
+
+        USE:
+        > from utils.log import Log
+        > _log = Log(filepath='path/to/file.log', autofill=True)
+        >
+        > _log.write_blank_line()
+        '''
+
+        try:
+
+            #APPEND BLANK LINE TO LOG FILE
+            with open(self._filepath, 'a') as f: f.write('\n')
+
+        except Exception as err:
+            #NOTIFICATION
+            reporterror.reporterror(err)
+
+
+#-----------------------------------------------------------------------
+# --- DEPRECIATED ---
+#-----------------------------------------------------------------------
+def write2log(filepath, text, autofill=True, printheader=False,
+              headertext=''):
+
+    '''
+    !!! WARNING !!!
+
+    This method has been depreciated and will be removed from future
+    versions of utils.
+
+    !!! WARNING !!!
+    '''
 
 
     try:
+
+        #DEPRECIATION NOTICE:
+        print '\nDEPRECIATION WARNING:\nThe write2log method has been depreciated and will ' \
+        'be removed in a future version of utils.\n\n' \
+        'ACTION:\nUpdate your code to use the new Log class, and its writing members.\n'
 
         #VALIDATION
         #TEST THE LOG FILE EXISTS (IF HEADER IS NOT REQUESTED)
