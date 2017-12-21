d63d99e4b51578ce4969deda556b46d54986c2b2
Jeremy Berendt <berendtj@RRDA.net>
2017-12-21 15:18:12 +0000

    6.0.0 - Database class additions.
    database.py
    -----------
    Added the MySQL() class
    Added the SQLite() class

--- utils/database.py
+++ utils/database.py
@@ -19,6 +19,8 @@ Use:        >>> import utils.database as db
 UPDATE LOG:
 Date        Programmer      Version     Update
 20.12.17    J. Berendt      0.1.0       Written  pylint (10/10)
+21.12.17                                Added the MySQL() class.
+                                        Added the SQLite() class.  pylint (10/10)
 ------------------------------------------------------------------------------------------------"""
 
 import utils
@@ -38,6 +40,7 @@ class Database(object):
     functions used by the more specific sub-classes.
     """
 
+    # ------------------------------------------------------------------
     def __init__(self):
 
         # INITIALISE
@@ -48,11 +51,9 @@ class Database(object):
 
     # ------------------------------------------------------------------
     def disconnect(self):
-
         """
-        SUMMARY:
-        A simple method to close the database connection, and change
-        the .connected property to False.
+        Disconnect a database connection, and change the .connected
+        property to False.
         """
 
         # CLOSE THE DATABASE CONNECTION
@@ -63,13 +64,9 @@ class Database(object):
     # ------------------------------------------------------------------
     @staticmethod
     def _parse_script(script_string):
-
         r"""
-        PUROSE:
-        Helper function used to parse a SQL script file into individual
-        statements.
-
-        The parsed statements are returned as a list.
+        Return a list of SQL statements which were parsed from the
+        script_string value.
 
         DESIGN:
         The design is very simple in that a list of statements is
@@ -104,16 +101,200 @@ class Database(object):
 
     # ------------------------------------------------------------------
     def _table_exists(self, sql):
-
         """
-        SUMMARY:
-        This general function tests if a table exists.  The result is
-        returned as a boolean value.
+        Run the passed sql statement to test if a table exists, and
+        return the result as a boolean value.
         """
 
         try:
             # RUN QUERY >> CONVERT RESULT TO BOOLEAN
             return bool(self.cur.execute(sql).fetchall()[0][0])
+
+        except Exception as err:
+            # USER NOTIFICATION
+            self._ui.print_error(err)
+
+
+# ----------------------------------------------------------------------
+class MySQL(Database):
+
+    """
+    PURPOSE:
+    This class is a lightweight wrapper for mysql-connector, and used
+    to handle database objects for MySQL.
+
+    PARAMETERS:
+    - host (default None)
+    IP address or machine name of the host or database server.
+    - database (default None)
+    The name of the database to be connected.
+    - user (default None)
+    Login: user name
+    - password (default None)
+    Login: password
+    - port (default 3306)
+    The TCP/IP port of the MySQL server; must be an integer.
+    - from_file (default False)
+    Load the database credentials from a config file.
+      - valid keys: host, database, port, user, password
+    - filename (default None)
+    Full path to the file holding the database credentials.
+
+    DEPENDENCIES:
+    - mysql-connector (2.1.4)
+    Installation: > pip install mysql-connector==2.1.4
+
+    USE:
+    > from utils.database import MySQL
+    >
+    > # LOAD CREDENTIALS
+    > sql = MySQL(host='my_host', database='my_db',
+                  user='my_user', password='my_pass')
+    >
+    > # CONNECT TO THE DB
+    > sql.connect()
+    > # CHECK CONNECTION WAS SUCCESSFUL
+    > sql.connected
+    > True
+    """
+
+    # ------------------------------------------------------------------
+    def __init__(self, host=None, database=None, user=None,
+                 password=None, port=3306, from_file=False,
+                 filename=None):
+
+        # INHERIT SUPER-CLASS PROPERTIES
+        super(MySQL, self).__init__()
+        # INITIALISE
+        self._host      = host
+        self._database  = database
+        self._user      = user
+        self._password  = password
+        self._port      = port
+        self._from_file = from_file
+        self._filename  = filename
+
+
+    # ------------------------------------------------------------------
+    def connect(self):
+        """
+        Connect to the database.  Update class properties accordingly.
+
+        DESIGN:
+        Function designed to create a connection to a MySQL database
+        using the provided login parameters, or directly from a config
+        file. If a login detail is not provided, the user is prompted;
+        which can be used as a security feature.
+
+        NOTE: To prompt for login details, leave the parameter(s)
+        blank.
+
+        Working in the background for the credential validation and
+        handling the actual database connection, are several other
+        functions which can be found in the utils module.
+
+        SOURCE:
+        The connection argument keys for a MySQL connection using the
+        mysql.connection library, are listed here:
+        https://dev.mysql.com/doc/connector-python/en/connector-python-connectargs.html
+        """
+
+        try:
+            # INITIALISE
+            creds = dict()
+            dbtype = 'mysql'
+
+            # TEST IF A CONFIG FILE IS TO BE USED
+            if self._from_file:
+                # DEFINE CREDENTIAL KEYS
+                db_keys = ['host', 'database', 'port', 'user', 'password']
+                # GET CREDENTIALS FROM THE CONFIG FILE
+                creds = utils._dbconn_fields(dbtype=dbtype, fields=db_keys,
+                                             filename=self._filename)
+            else:
+                # GET CREDENTIALS FROM PASSED PARAMETERS
+                creds = utils._dbconn_params(dbtype=dbtype, host=self._host,
+                                             database=self._database, user=self._user,
+                                             password=self._password)
+
+            # TEST IF CREDENTIALS ARE POPULATED
+            if creds is not None:
+                # CONNECT TO THE DATABASE >> UPDATE CLASS PROPERTIES
+                self._connect(creds=creds)
+
+        except Exception as err:
+            # USER NOTIFICATION
+            self._ui.print_alert('\nAn error occurred while connecting to the MySQL database.')
+            self._ui.print_error(err)
+
+
+    # ------------------------------------------------------------------
+    def table_exists(self, table_name):
+        """
+        Return a boolean value based on if a table exists.
+
+        DESIGN:
+        This function wraps the local _table_exists() function and
+        provides the SQL statement to be run.
+
+        PARAMETERS:
+        - table_name
+        The name of the table to be tested.
+        """
+
+        # BUILD SQL STATEMENT
+        sql = """
+        SELECT COUNT(*)
+        FROM INFORMATION_SCHEMA.TABLES
+        WHERE table_name = '%s'
+        LIMIT 1
+        """ % table_name
+
+        # GET >> RETURN BOOLEAN RESULT
+        return self._table_exists(sql=sql)
+
+
+    # ------------------------------------------------------------------
+    def _connect(self, creds):
+        """
+        Connect to the database using the passed credentials, update
+        class properties accordingly.
+
+        DESIGN:
+        The cursor is set with the buffered parameter set to True;
+        allowing the cursor to fetch all rows, rather than being
+        'lazily' loaded.
+
+        SOURCE: https://stackoverflow.com/a/33632767/6340496
+        """
+
+        try:
+            # MAKE THE CONNECTION
+            dbo = utils._dbconn_mysql_conn(creds=creds)
+            # SET CLASS PROPERTIES
+            self.conn       = dbo['conn']
+            self.cur        = self.conn.cursor(buffered=True)
+            self.connected  = True
+        except Exception:
+            self.connected = False
+
+
+    # ------------------------------------------------------------------
+    def _table_exists(self, sql):
+        """
+        Test if a table exists and return a boolean result.
+
+        DESIGN:
+        This function is used rather than the general _table_exists()
+        function as the MySQL cur.execute object does not have a
+        fetchall attribute, so these must be done in separate steps.
+        """
+
+        try:
+            # RUN QUERY
+            self.cur.execute(sql)
+            # GET >> CONVERT RESULT TO BOOLEAN AND RETURN
+            return bool(self.cur.fetchall()[0][0])
 
         except Exception as err:
             # USER NOTIFICATION
@@ -130,17 +311,17 @@ class Oracle(Database):
     properties.
 
     PARAMETERS:
-    - host
-    Database host; or database name
-    - user
+    - host (default None)
+    Database name, or TNS host
+    - user (default None)
     Login: user name
-    - password
+    - password (default None)
     Login: password
-    - from_file (default=False)
-    Load the database credentials from a config file
+    - from_file (default False)
+    Load the database credentials from a config file.
       - valid keys: host, user, password
-    - filename
-    Full path to the file holding the database credentials
+    - filename (default None)
+    Full path to the file holding the database credentials.
 
     USE:
     > from utils.database import Oracle
@@ -172,12 +353,8 @@ class Oracle(Database):
 
     # ------------------------------------------------------------------
     def connect(self):
-
         """
-        PURPOSE:
-        Method used to update the class' database connection properties.
-        The .connected property is a boolean value which can be used
-        by the caller to test if the connection was successful.
+        Connect to the database.  Update class properties accordingly.
 
         DESIGN:
         Function designed to create a connection to an Oracle
@@ -223,16 +400,14 @@ class Oracle(Database):
 
     # ------------------------------------------------------------------
     def execute_script(self, sql_file, commit=False):
-
         """
-        PURPOSE:
-        This method is designed specifically to handle a full Oracle
-        SQL script consisting of multiple SQL statements, as cx_Oracle
-        cannot natively handle this.
+        Execute a full SQL script on the database.
 
         DESIGN:
-        This method is a wrapper around cx_Oracle's execute() method,
-        and accepts the file path for the script to be executed.
+        This method is a wrapper around cx_Oracle's execute() method
+        and is designed specifically to handle a full Oracle
+        SQL script consisting of multiple SQL statements, as cx_Oracle
+        cannot natively handle this.
 
         Once the script is parsed into individual statements using
         the ';' character, each individual statement is run using
@@ -241,7 +416,7 @@ class Oracle(Database):
         PARAMETERS:
         - sql_file
         The full file path to the script to be run.
-        - commit (default=False)
+        - commit (default False)
         Boolean flag to execute a commit.
         """
 
@@ -270,27 +445,25 @@ class Oracle(Database):
 
     # ------------------------------------------------------------------
     def table_exists(self, table_name):
-
         """
-        SUMMARY:
-        This Oracle specific function tests if a table exists, and
-        returns a boolean value.
+        Return a boolean value based on if a table exists.
 
         DESIGN:
-        This function wraps the general _table_exists() function and
-        provides the SQL statement to be run.
+        This is an Oracle specific function which wraps the general
+        _table_exists() function and provides the SQL statement to be
+        run.
 
         PARAMETERS:
         - table_name
-        The name of the table to be tested
+        The name of the table to be tested.
         """
 
         # BUILD THE SQL STATEMENT
         sql = """
-        SELECT count(*) FROM dual WHERE EXISTS (
+        SELECT COUNT(*) FROM dual WHERE EXISTS (
             SELECT *
             FROM sys.user_tables
-            WHERE table_name = '%s'
+            WHERE upper(table_name) = '%s'
         )
         """ % table_name.upper()
 
@@ -300,11 +473,11 @@ class Oracle(Database):
 
     # ------------------------------------------------------------------
     def _execute_commit(self, sql, commit):
+        """Execute an SQL statement and commit if instructed.
 
-        """
-        SUMMARY:
-        Simple execute / commit wrapper around cx_Oracle's execute()
-        method.
+        DESIGN:
+        This is a simple execute / commit wrapper around cx_Oracle's
+        execute() method.
         """
 
         try:
@@ -322,11 +495,14 @@ class Oracle(Database):
 
     # ------------------------------------------------------------------
     def _connect(self, creds):
-
         """
-        SUMMARY:
-        Private method used to connnect to the database and set the
-        class properties.
+        Connect to the database using the passed credentials, update
+        class properties accordingly.
+
+        DESIGN:
+        In addition to setting the 'typical' class properties, a
+        connection string property has been added to this Oracle
+        specific connection.
         """
 
         try:
@@ -342,6 +518,97 @@ class Oracle(Database):
 
 
 # ----------------------------------------------------------------------
+class SQLite(Database):
+
+    """
+    PURPOSE:
+    This class is a lightweight wrapper for sqlite3, and used to handle
+    database objects for SQLite.
+
+    PARAMETERS:
+    - db_file_path
+    Full path to the SQLite database file.
+
+    USE:
+    > from utils.database import SQLite
+    >
+    > # LOAD CREDENTIALS
+    > sql = SQLite(db_file_path='path/to/data.db')
+    >
+    > # CONNECT TO THE DB
+    > sql.connect()
+    > # CHECK CONNECTION WAS SUCCESSFUL
+    > sql.connected
+    > True
+    """
+
+    # ------------------------------------------------------------------
+    def __init__(self, db_file_path):
+
+        # INHERIT SUPER-CLASS PROPERTIES
+        super(SQLite, self).__init__()
+        # INITIALISE
+        self._db_file_path = db_file_path
+
+
+    # ------------------------------------------------------------------
+    def connect(self):
+        """Connect to the SQLite database file."""
+
+        # TEST IF THE DB FILE EXISTS
+        if utils.fileexists(self._db_file_path):
+            # CONNECT
+            self._connect()
+
+
+    # ------------------------------------------------------------------
+    def table_exists(self, table_name):
+        """
+        Return a boolean value based on if a table exists.
+
+        DESIGN:
+        This is an SQLite specific function which wraps the general
+        _table_exists() function and provides the SQL statement to be
+        run.
+
+        PARAMETERS:
+        - table_name
+        The name of the table to be tested.
+        """
+
+        # BUILD THE SQL STATEMENT
+        sql = """
+        SELECT COUNT(*)
+        FROM sqlite_master
+        WHERE type = "table"
+        	AND upper(name) = '%s'
+        LIMIT 1
+
+        """ % table_name.upper()
+
+        # GET >> RETURN BOOLEAN RESULT
+        return self._table_exists(sql=sql)
+
+
+    # ------------------------------------------------------------------
+    def _connect(self):
+        """
+        Connect to the database file, update class properties
+        accordingly.
+        """
+
+        try:
+            # MAKE THE CONNECTION
+            dbo = utils.dbconn_sqlite(db_path=self._db_file_path)
+            # SET CLASS PROPERTIES
+            self.conn       = dbo['conn']
+            self.cur        = dbo['cur']
+            self.connected  = True
+        except Exception:
+            self.connected = False
+
+
+# ----------------------------------------------------------------------
 class SQLServer(Database):
 
     """
@@ -350,19 +617,19 @@ class SQLServer(Database):
     database objects for SQL Server.
 
     PARAMETERS:
-    - server
-    Name of the server where the SQL Server database lives
-    - database
-    The name of the database to be connected
-    - user
+    - server (default None)
+    Name of the server where the SQL Server database lives.
+    - database (default None)
+    The name of the database to be connected.
+    - user (default None)
     Login: user name
-    - password
+    - password (default None)
     Login: password
-    - from_file (default=False)
-    Load the database credentials from a config file
+    - from_file (default False)
+    Load the database credentials from a config file.
       - valid keys: server, database, user, password
-    - filename
-    Full path to the file holding the database credentials
+    - filename (default None)
+    Full path to the file holding the database credentials.
 
     USE:
     > from utils.database import SQLServer
@@ -395,12 +662,8 @@ class SQLServer(Database):
 
     # ------------------------------------------------------------------
     def connect(self):
-
         """
-        PURPOSE:
-        Method used to update the class' database connection properties.
-        The .connected property is a boolean value which can be used
-        by the caller to test if the connection was successful.
+        Connect to the database.  Update class properties accordingly.
 
         DESIGN:
         Function designed to create a connection to a SQL Server
@@ -447,26 +710,24 @@ class SQLServer(Database):
 
     # ------------------------------------------------------------------
     def table_exists(self, table_name):
-
         """
-        SUMMARY:
-        This SQL Server specific function tests if a table exists, and
-        returns a boolean value.
+        Return a boolean value based on if a table exists.
 
         DESIGN:
-        This function wraps the general _table_exists() function and
-        provides the SQL statement to be run.
+        This is a SQL Server specific function which wraps the general
+        _table_exists() function and provides the SQL statement to be
+        run.
 
         PARAMETERS:
         - table_name
-        The name of the table to be tested
+        The name of the table to be tested.
         """
 
         # BUILD SQL STATEMENT
         sql = """
-        SELECT count(*)
+        SELECT COUNT(*)
         FROM INFORMATION_SCHEMA.TABLES
-        WHERE table_name = '%s'
+        WHERE upper(table_name) = '%s'
         """ % table_name.upper()
 
         # GET >> RETURN BOOLEAN RESULT
@@ -475,11 +736,9 @@ class SQLServer(Database):
 
     # ------------------------------------------------------------------
     def _connect(self, creds):
-
         """
-        SUMMARY:
-        Private method used to connnect to the database and set the
-        class properties.
+        Connect to the database using the passed credentials, update
+        class properties accordingly.
         """
 
         try:
